9ea54ce02f156fee049c57602e0d54ad35bda0d2
LukePadawalker <padurariu.luca@gmail.com>
2024-01-16 15:01:45 +0100

    snack 2 done

--- script.js
+++ script.js
@@ -43,3 +43,76 @@ console.log(guestsList);
 // 3.  dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);
 
 
+
+const facolta = [
+  {
+    id: "213",
+    name: "Marco della Rovere",
+    grades: "78"
+  },
+  {
+    id: "110",
+    name: "Paola Cortellessa",
+    grades: "96"
+  },
+  {
+    id: "250",
+    name: "Andrea Mantegna",
+    grades: "48"
+  },
+  {
+    id: "145",
+    name: "Gaia Borromini",
+    grades: "74"
+  },
+  {
+    id: "196",
+    name: "Luigi Grimaldello",
+    grades: "68"
+  },
+  {
+    id: "102",
+    name: "Piero della Francesca",
+    grades: "50"
+  },
+  {
+    id: "120",
+    name: "Francesca da Polenta",
+    grades: "84"
+  }
+]
+
+// 1. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+// filter
+
+let upperSeventy = facolta.filter(student => student.grades >= "70")
+console.log(upperSeventy);
+
+
+// 2.Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+// filter
+
+let upperId = facolta.filter(student => student.grades >= "70" && student.id >= "120")
+console.log(upperId);
+
+
+// 3.  dobbiamo stampare le targhe col nome degli studenti: creare una lista contenente il loro nome tutto in maiuscolo ES (Marco della Rovere => MARCO DELLA ROVERE);
+// map
+
+let upperCase = facolta.map(student => student.name.toUpperCase())
+console.log(upperCase);
+
+
+
+
+
+
+
+// Id|Name|Grades
+// 213|Marco della Rovere|78
+// 110|Paola Cortellessa|96
+// 250|Andrea Mantegna|48
+// 145|Gaia Borromini|74
+// 196|Luigi Grimaldello|68
+// 102|Piero della Francesca|50
+// 120|Francesca da Polenta|84
